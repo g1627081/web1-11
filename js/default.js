4be65a03c89f3eb177076793c3fb0ed954338af2
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-19 17:24:30 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,15 @@
 ver number = Math.floor(Math.random()*3);
-ver answer = perseInt(window.prompt(`数あてゲーム。0～2の数字を入力してください。`));
+ver answer = perseInt(window.prompt('数あてゲーム。0～2の数字を入力してください。'));
 
 ver message;
 if(answer === number){
-  message = `あたり！`;
+  message = 'あたり！';
 }else if (answer < number){
-  message = `残念！もっと大きいです`;
+  message = '残念！もっと大きいです';
 }else if (number < answer){
-  message = `残念！もっと小さいです`;
+  message = '残念！もっと小さいです';
 }else{
-  message = `0~3の数字を入力してください`;
+  message = '0~3の数字を入力してください';
 }
 
-document.getElementById(`choice`).textContent = message;
+document.getElementById('choice').textContent = message;
